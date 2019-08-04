252afc1a59ddd5ce13bc40d9d8ffeaa9209edcc9
Rj Tiglao <rjtiglao@gmail.com>
2019-08-03 19:20:30 -0700

    logic prepends gifs

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,29 +1,42 @@
-// API VALIDATIONS
-let searchQuery = "Simpsons";
-let key = "wNn70PU5687UBNk6pNoCSTZg4wtcdzg1";
-let queryUrl = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${searchQuery}&limit=10&offset=0&rating=G&lang=en`
+// // API VALIDATIONS
+// let searchQuery = "Simpsons";
+// let key = "wNn70PU5687UBNk6pNoCSTZg4wtcdzg1";
+// let queryUrl = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${searchQuery}&limit=10&offset=0&rating=G&lang=en`
 
 
+let func = function(e){
 
-// let buildQuery= function(){
+    let searchQuery = e.getAttribute(`value`);
+    let key = "wNn70PU5687UBNk6pNoCSTZg4wtcdzg1";
+    let queryUrl = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${searchQuery}&limit=10&offset=0&rating=G&lang=en`
 
-
-
-
-
-// }
-fetch(queryUrl)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    let createImg = document.createElement(`img`);
-    console.log(myJson["data"][0]["images"]["fixed_height_small"]["url"]);
-    // createImg.setAttribute(`value`,`${myJson["data"][0]["title"]}`);
-    createImg.src = `${myJson["data"][0]["images"]["fixed_height_small"]["url"]}`;
-    document.getElementById("gifContainer").appendChild(createImg);
+    fetch(queryUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        
+        for(i=0;i<10; i++){
+            let createImg = document.createElement(`img`);
+            console.log(myJson["data"][i]["images"]["fixed_height_small"]["url"]);
+            // createImg.setAttribute(`value`,`${myJson["data"][0]["title"]}`);
+            createImg.src = `${myJson["data"][i]["images"]["fixed_height_small"]["url"]}`;
+            document.getElementById("gifContainer").prepend(createImg);
+        }
+    });
+}
+// fetch(queryUrl)
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(myJson) {
+//     let createImg = document.createElement(`img`);
+//     console.log(myJson["data"][0]["images"]["fixed_height_small"]["url"]);
+//     // createImg.setAttribute(`value`,`${myJson["data"][0]["title"]}`);
+//     createImg.src = `${myJson["data"][0]["images"]["fixed_height_small"]["url"]}`;
+//     document.getElementById("gifContainer").appendChild(createImg);
  
-  });
+//   });
 
 
 
@@ -37,15 +50,6 @@ let count = 0;
 
 //FUNCTIONS
 
-//Display image
-document.getElementById("submit").onclick = function(){
-let createImg = document.createElement(`img`);
-createButtons.setAttribute(`value`,`${myJson["data"][0]}`);
-document.getElementById("buttonContainer").appendChild(createButtons);
-document.getElementById(`button${count}${topics[i]}`).innerHTML = `${topics[i]}`;   
-count++ 
-}
-
 
 // Creates a button for each topic
 window.onload = function(){
@@ -55,6 +59,7 @@ window.onload = function(){
         createButtons.id = `button${count}${topics[i]}`;
         createButtons.className = `topic-button`;
         createButtons.setAttribute(`value`,`${topics[i]}`);
+        createButtons.setAttribute(`onclick`,`func(this)`);
         document.getElementById("buttonContainer").appendChild(createButtons);
         document.getElementById(`button${count}${topics[i]}`).innerHTML = `${topics[i]}`;   
         count++ 
@@ -68,68 +73,9 @@ document.getElementById("submit").onclick = function(){
     let createButtons = document.createElement(`button`);
     createButtons.id = `button${count}${userInput}`;
     createButtons.className = `topic-button`;
+    createButtons.setAttribute(`onclick`,`func(this)`);
     createButtons.setAttribute(`value`,`${userInput}`);
     document.getElementById("buttonContainer").appendChild(createButtons);
     document.getElementById(`button${count}${userInput}`).innerHTML = `${userInput}`;  
     count++  
 };
-
-
-
-
-
-// Try using a loop that appends a button for each string in the array.
-// When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
-
-// When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
-
-// Under every gif, display its rating (PG, G, so on).
-
-// This data is provided by the GIPHY API.
-// Only once you get images displaying with button presses should you move on to the next step.
-// Add a form to your page that takes a value from a user input box and adds it to your topics array. Then make a function call that takes each topic in the array and remakes the buttons on the page.
-
-// Deploy your assignment to Github Pages.
-
-
-
-
-// $(".gif").on("click", function() {
-//     // STEP ONE: study the html above.
-//     // Look at all the data attributes.
-//     // Run the file in the browser. Look at the images.
-
-//     // After we complete steps 1 and 2 we'll be able to pause gifs from giphy.
-
-//     // STEP TWO: make a variable named state and then store the image's data-state into it.
-//     // Use the .attr() method for this.
-
-//     // // ============== FILL IN CODE HERE FOR STEP TWO =========================
-//     let state = $(this).attr('data-state');
-    
-
-//     // =============================================
-
-//     // STEP THREE: Check if the variable state is equal to 'still',
-//     // then update the src attribute of this image to it's data-animate value,
-//     // and update the data-state attribute to 'animate'.
-  
-//     if(state==="still"){
-//       $(this).attr("data-state",'animate');
-//       $(this).attr("src",$(this).attr('data-animate'));
-
-//     }else{
-//       let still = $(this).attr
-//     }
-
-//     // If state is equal to 'animate', then update the src attribute of this
-//     // image to it's data-still value and update the data-state attribute to 'still'
-//     // ============== FILL IN CODE HERE FOR STEP THREE =========================
-
-//     // CODE GOES HERE
-
-//     // ==============================================
-
-//     // STEP FOUR: open the file in the browser and click on the images.
-//     // Then click again to pause.
-//   });
